a0e333ecfe90b85f5031be9a7c735e568b81ba47
CORP\Shiva.Marimallaiah <shivaprasadcool06@gmail.com>
2017-01-31 16:31:26 +0530

    TimeRange

---
+++ IntersectionOfDates/Class1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntersectionOfDates
+{
+    class Program
+    {
+        public void IntersectPeriods(List<dynamic> SetA, List<dynamic> SetB)
+        {
+            foreach (TimeRange PeriodSetA in SetA)
+            {
+                foreach (TimeRange PeriodSetB in SetB)
+                {
+                    if ((PeriodSetA.EndingDate > PeriodSetA.StartingDate) && (PeriodSetB.EndingDate > PeriodSetB.StartingDate))
+                    {
+                        if (((PeriodSetA.StartingDate <= PeriodSetB.StartingDate) && (PeriodSetA.EndingDate <= PeriodSetB.EndingDate)) && ((PeriodSetA.EndingDate >= PeriodSetB.StartingDate)))
+                            Console.WriteLine(PeriodSetB.StartingDate + " - " + PeriodSetA.EndingDate);
+                        else if ((PeriodSetA.StartingDate >= PeriodSetB.StartingDate) && (PeriodSetA.EndingDate >= PeriodSetB.EndingDate) && ((PeriodSetA.EndingDate >= PeriodSetB.StartingDate)))
+                            Console.WriteLine(PeriodSetA.StartingDate + " - " + PeriodSetB.EndingDate);
+                        else if ((PeriodSetA.StartingDate >= PeriodSetB.StartingDate) && (PeriodSetA.EndingDate <= PeriodSetB.EndingDate))
+                            Console.WriteLine(PeriodSetA.StartingDate + " - " + PeriodSetA.EndingDate);
+                        else if ((PeriodSetA.StartingDate <= PeriodSetB.StartingDate) && (PeriodSetA.EndingDate >= PeriodSetB.EndingDate))
+                            Console.WriteLine(PeriodSetB.StartingDate + " - " + PeriodSetB.EndingDate);
+                        else
+                            Console.WriteLine("No Intersection");
+                    }
+                    else
+                        Console.WriteLine("Wrong Entry");
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Program Range = new Program();
+            var ListA = new List<dynamic>();
+            var ListB = new List<dynamic>();
+            Console.WriteLine("Enter the number of periods in setA");
+            int NumberOfPeriodsSetA = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the number of periods in setB");
+            int NumberOfPeriodSetB = Convert.ToInt32(Console.ReadLine());
+            for (int index = 0; index < NumberOfPeriodsSetA; index++)
+            {
+                Console.WriteLine("Enter your period range {0} in setA: ", (index + 1));
+                TimeRange p1 = new TimeRange(Convert.ToDateTime(Console.ReadLine()), Convert.ToDateTime(Console.ReadLine()));
+                ListA.Add(p1);
+            }
+            for (int index = 0; index < NumberOfPeriodSetB; index++)
+            {
+                Console.WriteLine("Enter your period range {0} in setB: ", (index + 1));
+                TimeRange p2 = new TimeRange(Convert.ToDateTime(Console.ReadLine()), Convert.ToDateTime(Console.ReadLine()));
+                ListB.Add(p2);
+            }
+            Range.IntersectPeriods(ListA, ListB);
+            Console.ReadLine();
+        }
+    }
+
+}
+
+
---
+++ IntersectionOfDates/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntersectionOfDates
+{
+   class TimeRange
+        {
+            public DateTime StartingDate { get; set; }
+
+            public DateTime EndingDate { get; set; }
+
+            public TimeRange(DateTime start, DateTime end)
+            {
+                this.StartingDate = start;
+                this.EndingDate = end;
+            }
+
+            public TimeRange()
+            {
+
+            }
+        }
+
+
+    }
+
+
